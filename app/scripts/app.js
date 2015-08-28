6d516fa1103c7db91ba15e2727be349af02e7d6d
Gourav Anvekar <gourav.anvekar@gmail.com>
2015-08-28 15:00:29 -0400

    Assignment 3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,21 @@
-var myAppModule = angular.module('myApp', []);
+var myAppModule = angular.module('myApp', ['ui.router']);
+myAppModule.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
+    $stateProvider
+        .state('landing', {
+            url: "/",
+            templateUrl: "../templates/landing.html"
+        })
+        .state('collection', {
+            url: "/collection",
+            templateUrl: "../templates/collection.html"
+        })
+        .state('album', {
+            url: "/album",
+            templateUrl: "../templates/album.html"
+        })
+});
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,37 @@
-var myAppModule = angular.module('myApp', []);
+var myAppModule = angular.module('myApp', ['ui.router']);
+myAppModule.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    //$urlRouterProvider.otherwise("/");
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
+    $stateProvider
+        .state('landing', {
+            url: "/",
+            templateUrl: "../templates/landing.html"
+        })
+        .state('collection', {
+            url: "/collection",
+            templateUrl: "../templates/collection.html"
+        })
+        .state('album', {
+            url: "/album",
+            templateUrl: "../templates/album.html"
+        })
+});
+
+myAppModule.factory('coolthings', function () {
+    return { coolThings:
+        [
+    {name: 'fiorst cool thing'},
+    {name: 'second cool thing'}
+        ]
+    }
+})
+
+myAppModule.controller('album', ['$scope, $interval, coolthings', function ($scope, $interval, coolthings) {
+    $scope.albumPicaso = albumPicaso;
+    $scope.coolthings = coolthings.coolThings;
+}]);
